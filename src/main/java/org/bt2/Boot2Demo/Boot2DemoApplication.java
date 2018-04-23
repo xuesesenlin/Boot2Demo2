64e2a8900dd1e9677bbeefe63046081e245d790b
xuesemofa@126.com <xuesemofa@126.com>
2018-04-23 09:46:41 +0800

    首次提交

---
+++ src/main/java/org/bt2/Boot2Demo/Boot2DemoApplication.java
@@ -0,0 +1,15 @@
+package org.bt2.Boot2Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot2
+ */
+@SpringBootApplication
+public class Boot2DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot2DemoApplication.class, args);
+    }
+}
